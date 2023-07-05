367660b49d4a1dab45f42571a84a61414fdc70f0
Ryder <ra302211@gmail.com>
2023-07-05 12:01:54 -0500

    kick command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,4 +1,6 @@
 from discord.ext import commands
+from start_bot import SERVER
+from base import DISCORD_ROLES
 import os, pathlib, json
 
 DATA_DIR = './data/moderation/'
@@ -6,6 +8,25 @@ DATA_DIR = './data/moderation/'
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.server = bot.get_guild(SERVER)
+
+    async def kick(self, id, reason:str):
+        with open(f'{DATA_DIR}kicked' , 'a') as file:
+            kick = {id: {'reason':reason}}
+            json.dump(kick, file)
+        await self.server.kick(self.server.fetch_member(id), reason)
+
+    @commands.has_role(DISCORD_ROLES[0])
+    @commands.command()
+    async def mod(self, ctx, arg=None):
+        match arg[0]:
+            case 'kick':
+                await ctx.message.delete()
+                await self.kick(arg[1])
+            case '_':
+                await ctx.message.delete()
+                await ctx.author.send('Test success')
+
 
 
 async def setup(client):
